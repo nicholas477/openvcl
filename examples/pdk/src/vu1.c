583cf8df63e0e8fe35ca03274e8c275e14bc85d6
nchalkley2 <nicholas2.chalkley@gmail.com>
2023-11-26 13:18:53 -0500

    Fixed compilation errors in example

--- examples/pdk/src/vu1.c
+++ examples/pdk/src/vu1.c
@@ -9,7 +9,7 @@
  */
 
 #include <tamtypes.h>
-#include <fileio.h>
+#include <fcntl.h>
 #include <kernel.h>
 #include <string.h>
 #include "vu1.h"
@@ -19,150 +19,175 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static char dmaBuffer1[100*1024] __attribute__((aligned(16)));
-static char dmaBuffer2[100*1024] __attribute__((aligned(16)));
+static char dmaBuffer1[100 * 1024] __attribute__((aligned(16)));
+static char dmaBuffer2[100 * 1024] __attribute__((aligned(16)));
 static void* currentBuffer;
-static int   switchBuffer = 0;
+static int switchBuffer = 0;
 static pdkVu1ListBuild buildList;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1UploadProg(int Dest, void* start, void* end)
 {
-  int   count = 0;
-  u8 tempDma[512] ALIGNED(16);
-  void* chain = (u64*)&tempDma; // uncached
+	int count = 0;
+	u8 tempDma[512] ALIGNED(16);
+	void* chain = (u64*)&tempDma; // uncached
 
-  // get the size of the code as we can only send 256 instructions in each MPGtag
+	// get the size of the code as we can only send 256 instructions in each MPGtag
 
-  count = pdkVu1Size( start, end ); 
+	count = pdkVu1Size(start, end);
 
-  while( count > 0 )
-  {
-    u32 current_count = count > 256 ? 256 : count;
-    
-    *((u64*) chain)++ = DMA_REF_TAG( (u32)start, current_count/2 );
-    *((u32*) chain)++ = VIF_CODE( VIF_NOP,0,0 );
-    *((u32*) chain)++ = VIF_CODE( VIF_MPG,current_count&0xff,Dest );
+	while (count > 0)
+	{
+		u32 current_count = count > 256 ? 256 : count;
 
-    start += current_count*2;
-    count -= current_count; 
-    Dest += current_count;
-  }
+		*((u64*)chain) = DMA_REF_TAG((u32)start, current_count / 2);
+		chain          = (u64*)chain + 1;
+		*((u32*)chain) = VIF_CODE(VIF_NOP, 0, 0);
+		chain          = (u32*)chain + 1;
+		*((u32*)chain) = VIF_CODE(VIF_MPG, current_count & 0xff, Dest);
+		chain          = (u32*)chain + 1;
 
-  *((u64*) chain)++ = DMA_END_TAG( 0 );
-  *((u32*) chain)++ = VIF_CODE(VIF_NOP,0,0);
-  *((u32*) chain)++ = VIF_CODE(VIF_NOP,0,0);
+		start += current_count * 2;
+		count -= current_count;
+		Dest += current_count;
+	}
 
-  // Send it to vif1
-  FlushCache(0);
-  pdkDma01SendChain(tempDma);
+	*((u64*)chain) = DMA_END_TAG(0);
+	chain          = (u64*)chain + 1;
+	*((u32*)chain) = VIF_CODE(VIF_NOP, 0, 0);
+	chain          = (u32*)chain + 1;
+	*((u32*)chain) = VIF_CODE(VIF_NOP, 0, 0);
+	chain          = (u32*)chain + 1;
+
+	// Send it to vif1
+	FlushCache(0);
+	pdkDma01SendChain(tempDma);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListBegin()
 {
-  switchBuffer = !switchBuffer;
+	switchBuffer = !switchBuffer;
 
-  memset((char*)&buildList,0,sizeof(buildList));
+	memset((char*)&buildList, 0, sizeof(buildList));
 
-  if(switchBuffer)
-    currentBuffer = (char*)&dmaBuffer1;
-  else
-    currentBuffer = (char*)&dmaBuffer2;
+	if (switchBuffer)
+		currentBuffer = (char*)&dmaBuffer1;
+	else
+		currentBuffer = (char*)&dmaBuffer2;
 
-  buildList.kickBuffer = currentBuffer;
+	buildList.kickBuffer = currentBuffer;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void pdkVu1ListData(int destAdress,void* data,int quadSize)
+void pdkVu1ListData(int destAdress, void* data, int quadSize)
 {
-  if(((u32)data & 0xf))
-  {
-    pdkOut("Data to pdkVu1ListAddData is not 16byte aligned!, data WILL be wrong\n");
-  }
+	if (((u32)data & 0xf))
+	{
+		pdkOut("Data to pdkVu1ListAddData is not 16byte aligned!, data WILL be wrong\n");
+	}
 
-  *((u64*)currentBuffer)++ = DMA_REF_TAG( (u32)data, quadSize );
-  *((u32*)currentBuffer)++ = VIF_CODE( VIF_STCYL,0,0x0101 );
-  *((u32*)currentBuffer)++ = VIF_CODE( VIF_UNPACK_V4_32,quadSize,destAdress );
+	*((u64*)currentBuffer) = DMA_REF_TAG((u32)data, quadSize);
+	currentBuffer          = (u64*)currentBuffer + 1;
+	*((u32*)currentBuffer) = VIF_CODE(VIF_STCYL, 0, 0x0101);
+	currentBuffer          = (u32*)currentBuffer + 1;
+	*((u32*)currentBuffer) = VIF_CODE(VIF_UNPACK_V4_32, quadSize, destAdress);
+	currentBuffer          = (u32*)currentBuffer + 1;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListAddBegin(int adress)
 {
-  if(buildList.isBuidingCnt == 1)
-    pdkOut("pdkVu1ListAddBegin: Already building dmalist!\n");
+	if (buildList.isBuidingCnt == 1)
+		pdkOut("pdkVu1ListAddBegin: Already building dmalist!\n");
 
-  buildList.cntDmaDest = adress;
-  buildList.offset = currentBuffer;
-  *((u64*)currentBuffer)++ = DMA_CNT_TAG(0);
-  *((u32*)currentBuffer)++ = VIF_CODE(VIF_STCYL,0,0x0101 );
-  *((u32*)currentBuffer)++ = VIF_CODE(VIF_UNPACK_V4_32,0,0);
-  buildList.isBuidingCnt = 1;
+	buildList.cntDmaDest   = adress;
+	buildList.offset       = currentBuffer;
+	*((u64*)currentBuffer) = DMA_CNT_TAG(0);
+	currentBuffer          = (u64*)currentBuffer + 1;
+	*((u32*)currentBuffer) = VIF_CODE(VIF_STCYL, 0, 0x0101);
+	currentBuffer          = (u32*)currentBuffer + 1;
+	*((u32*)currentBuffer) = VIF_CODE(VIF_UNPACK_V4_32, 0, 0);
+	currentBuffer          = (u32*)currentBuffer + 1;
+	buildList.isBuidingCnt = 1;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void pdkVu1ListAdd128(u64 v1,u64 v2)
+void pdkVu1ListAdd128(u64 v1, u64 v2)
 {
-  pdkVu1ListCheckCnt();
+	pdkVu1ListCheckCnt();
 
-  *((u64*)currentBuffer)++ = v1;
-  *((u64*)currentBuffer)++ = v2;
+	*((u64*)currentBuffer) = v1;
+	currentBuffer          = (u64*)currentBuffer + 1;
+	*((u64*)currentBuffer) = v2;
+	currentBuffer          = (u64*)currentBuffer + 1;
 
-  buildList.dmaSize += 16;
+	buildList.dmaSize += 16;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListAdd64(u64 v)
 {
-  pdkVu1ListCheckCnt();
+	pdkVu1ListCheckCnt();
 
-  *((u64*)currentBuffer)++ = v;
+	*((u64*)currentBuffer) = v;
+	currentBuffer          = (u64*)currentBuffer + 1;
 
-  buildList.dmaSize += 8;
+	buildList.dmaSize += 8;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListAdd32(u32 v)
 {
-  pdkVu1ListCheckCnt();
+	pdkVu1ListCheckCnt();
 
-  *((u32*)currentBuffer)++ = v;
+	*((u32*)currentBuffer) = v;
+	currentBuffer          = (u32*)currentBuffer + 1;
 
-  buildList.dmaSize += 4;
+	buildList.dmaSize += 4;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListAddFloat(float v)
 {
-  pdkVu1ListCheckCnt();
+	pdkVu1ListCheckCnt();
 
-  *((float*)currentBuffer)++ = v;
+	*((float*)currentBuffer) = v;
+	currentBuffer            = (float*)currentBuffer + 1;
 
-  buildList.dmaSize += 4;
+	buildList.dmaSize += 4;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListEnd(int start)
 {
-  *((u64*)currentBuffer)++ = DMA_END_TAG(0);
-  *((u32*)currentBuffer)++ = VIF_CODE(VIF_NOP,0,0);
+	*((u64*)currentBuffer) = DMA_END_TAG(0);
+	currentBuffer          = (u64*)currentBuffer + 1;
+	*((u32*)currentBuffer) = VIF_CODE(VIF_NOP, 0, 0);
+	currentBuffer          = (u32*)currentBuffer + 1;
 
-  if(start>=0)
-    *((u32*)currentBuffer)++ = VIF_CODE(VIF_MSCAL,0,start);
-  else
-    *((u32*)currentBuffer)++ = VIF_CODE(VIF_NOP,0,0);
+	if (start >= 0)
+	{
+		*((u32*)currentBuffer) = VIF_CODE(VIF_MSCAL, 0, start);
+		currentBuffer          = (u32*)currentBuffer + 1;
+	}
+	else
+	{
+		*((u32*)currentBuffer) = VIF_CODE(VIF_NOP, 0, 0);
+		currentBuffer          = (u32*)currentBuffer + 1;
+	}
 
-  pdkDmaWait(1);
-  pdkDma01SendChain(buildList.kickBuffer);
+	pdkDmaWait(1);
+	pdkDma01SendChain(buildList.kickBuffer);
 }
 
 
@@ -170,45 +195,49 @@ void pdkVu1ListEnd(int start)
 
 void pdkVu1ListCheckCnt()
 {
-  if(buildList.isBuidingCnt == 0)
-    pdkOut("pdkVu1ListCheckCnt: pdkVu1ListAddBegin not called\n");
+	if (buildList.isBuidingCnt == 0)
+		pdkOut("pdkVu1ListCheckCnt: pdkVu1ListAddBegin not called\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1ListAddEnd()
 {
-  if(buildList.isBuidingCnt == 0)
-  {
-    pdkOut("pdkVu1ListAddEnd: pdkVu1ListAddBegin was not called, nothing to end!\n");
-    return;
-  }
-  
-  // pad to qword alignment
-  while((buildList.dmaSize & 0xf))
-  {
-    *((u32*)currentBuffer)++ = 0;    
-    buildList.dmaSize += 4;
-  }
-    
-  *((u64*)buildList.offset)++ = DMA_CNT_TAG(buildList.dmaSize >> 4);
-  *((u32*)buildList.offset)++ = VIF_CODE(VIF_STCYL,0,0x0101 );
-  *((u32*)buildList.offset)++ = VIF_CODE(VIF_UNPACK_V4_32,buildList.dmaSize >> 4,buildList.cntDmaDest);
-  
-  buildList.isBuidingCnt = 0;
+	if (buildList.isBuidingCnt == 0)
+	{
+		pdkOut("pdkVu1ListAddEnd: pdkVu1ListAddBegin was not called, nothing to end!\n");
+		return;
+	}
+
+	// pad to qword alignment
+	while ((buildList.dmaSize & 0xf))
+	{
+		*((u32*)currentBuffer) = 0;
+		currentBuffer          = (u32*)currentBuffer + 1;
+		buildList.dmaSize += 4;
+	}
+
+	*((u64*)buildList.offset) = DMA_CNT_TAG(buildList.dmaSize >> 4);
+	currentBuffer             = (u64*)currentBuffer + 1;
+	*((u32*)buildList.offset) = VIF_CODE(VIF_STCYL, 0, 0x0101);
+	currentBuffer             = (u32*)currentBuffer + 1;
+	*((u32*)buildList.offset) = VIF_CODE(VIF_UNPACK_V4_32, buildList.dmaSize >> 4, buildList.cntDmaDest);
+	currentBuffer             = (u32*)currentBuffer + 1;
+
+	buildList.isBuidingCnt = 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int pdkVu1Size(u32* start, u32* end)
 {
-  int size = (end-start)/2;
+	int size = (end - start) / 2;
 
 	// if size is odd we have make it even in order for the transfer to work
 	// (quadwords, because of that its VERY important to have an extra nop nop
 	// at the end of each vuprogram
 
-	if( size&1 )
+	if (size & 1)
 		size++;
 
 	return size;
@@ -216,19 +245,19 @@ int pdkVu1Size(u32* start, u32* end)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int FileWrite( const char* pName, void* pData, int size )
+int FileWrite(const char* pName, void* pData, int size)
 {
 	int fd;
-	fd = fioOpen( pName, O_CREAT|O_WRONLY );
-	
-	if( fd > 0 )
+	fd = open(pName, O_CREAT | O_WRONLY);
+
+	if (fd > 0)
 	{
-		if( fioWrite( fd, pData, size) != size )
+		if (write(fd, pData, size) != size)
 		{
-			fioClose(fd);
+			close(fd);
 			SleepThread();
 		}
-		fioClose(fd);
+		close(fd);
 	}
 	else
 	{
@@ -236,7 +265,7 @@ int FileWrite( const char* pName, void* pData, int size )
 		SleepThread();
 	}
 
-  return -1;
+	return -1;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -244,29 +273,27 @@ int FileWrite( const char* pName, void* pData, int size )
 void pdkVu1Dump()
 {
 	asm __volatile__(
-		"nop\n"
-		"nop\n"
-"0:\n"
-		"bc2t 0b\n"
-		"nop\n"
-		"nop\n" );
-  
-  FileWrite( "host:VU1MICROMEM",((void*)0x11008000),1024*16 );
-  FileWrite( "host:VU1MEM", ((void*)0x1100c000),1024*16 );
+	    "nop\n"
+	    "nop\n"
+	    "0:\n"
+	    "bc2t 0b\n"
+	    "nop\n"
+	    "nop\n");
+
+	FileWrite("host:VU1MICROMEM", ((void*)0x11008000), 1024 * 16);
+	FileWrite("host:VU1MEM", ((void*)0x1100c000), 1024 * 16);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void pdkVu1DumpOnce()
 {
-  static int hasDUmped = 0;
+	static int hasDUmped = 0;
 
-  if(hasDUmped == 0)
-  {
-    pdkDmaWait(1);
-    pdkVu1Dump();
-    hasDUmped = 1;
-  }
+	if (hasDUmped == 0)
+	{
+		pdkDmaWait(1);
+		pdkVu1Dump();
+		hasDUmped = 1;
+	}
 }
-
-
